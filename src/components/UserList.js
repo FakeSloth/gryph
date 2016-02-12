bda7dfedd5bfa50148abe72efb0df5094a0d1b38
Phil <birkal@outlook.com>
2016-02-11 20:52:51 -0800

    Sort on all cases

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -14,6 +14,7 @@ class UserList extends Component {
   render() {
     const users = this.props.users;
     users.sort((a, b) => {
+      a = a.toLowerCase(), b = b.toLowerCase();
       if (a < b) return -1;
       if (a > b) return 1;
       return 0;
