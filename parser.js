8648e9fd875f4039e6874de0dd62867549a85242
thefenderstory <jgrady15@brophybroncos.org>
2016-02-13 14:31:06 -0700

    Use function to parse emails

--- parser.js
+++ parser.js
@@ -3,6 +3,15 @@
 const demFeels = require('dem-feels');
 const escapeHTML = require('./utils').escapeHTML;
 
+function validateEmail(str) {
+  let reg = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/;
+  if (reg.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function parser(str) {
   // escape html
   str = escapeHTML(str);
@@ -12,15 +21,18 @@ function parser(str) {
   str = str.replace(/\_\_([^< ](?:[^<]*?[^< ])??)\_\_(?![^<]*?<\/a)/g, '<i>$1</i>');
   // **bold**
   str = str.replace(/\*\*([^< ](?:[^<]*?[^< ])??)\*\*/g, '<b>$1</b>');
+  //email addresses
+  if(validateEmail(str)) {
+    str = str.replace(/[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim, '<a href="mailto:$&">$&</a>');
+    return str;
+  }
   // linking of URIs
   str = str
     .replace(/(https?\:\/\/[a-z0-9-.]+(\/([^\s]*[^\s?.,])?)?|[a-z0-9]([a-z0-9-\.]*[a-z0-9])?\.(com|org|net|edu|tk|us|io|me)((\/([^\s]*[^\s?.,])?)?|\b))/ig, '<a href="$1" target="_blank">$1</a>')
     .replace(/<a href="([a-z]*[^a-z:])/g, '<a href="http://$1').replace(/(\bgoogle ?\[([^\]<]+)\])/ig, '<a href="http://www.google.com/search?ie=UTF-8&q=$2" target="_blank">$1</a>')
     .replace(/(\bgl ?\[([^\]<]+)\])/ig, '<a href="http://www.google.com/search?ie=UTF-8&btnI&q=$2" target="_blank">$1</a>')
     .replace(/(\bwiki ?\[([^\]<]+)\])/ig, '<a href="http://en.wikipedia.org/w/index.php?title=Special:Search&search=$2" target="_blank">$1</a>')
-    .replace(/\[\[([^< ]([^<`]*?[^< ])?)\]\]/ig, '<a href="http://www.google.com/search?ie=UTF-8&btnI&q=$1" target="_blank">$1</a>')
-    // email addresses
-    .replace(/[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim, '<a href="mailto:$&">$&</a>');
+    .replace(/\[\[([^< ]([^<`]*?[^< ])?)\]\]/ig, '<a href="http://www.google.com/search?ie=UTF-8&btnI&q=$1" target="_blank">$1</a>');
   // emotes
   str = demFeels(str);
 
