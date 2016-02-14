101c52929377f84a7ededb19615d46222d1a771e
Phil <birkal@outlook.com>
2016-02-13 16:12:13 -0800

    Fix duplicate key and wrong selector

--- components/VideoHistoryList.js
+++ components/VideoHistoryList.js
@@ -9,7 +9,7 @@ class VideoHistoryList extends Component {
   render() {
     const length = this.props.videos.length;
     const list = this.props.videos.map((video, index) => (
-      <div className="media">
+      <div className="media" key={index}>
         <div className="media-left">
           <a href={video.url}>
             <img className="media-object" src={video.img} alt={video.title} />
@@ -30,9 +30,9 @@ class VideoHistoryList extends Component {
         </div>
       </div>
     ));
-    let style = {'overflow-y': 'auto', 'height': '40vh'};
+    let style = {'overflowY': 'auto', 'height': '40vh'};
     if (this.props.isPlaying) {
-      style = {'overflow-y': 'auto', 'height': '10vh'};
+      style = {'overflowY': 'auto', 'height': '10vh'};
     }
     return (
       <div ref="list">
