e5260a918576eaffe43698918985f3d8c35ffe76
Phil <birkal@outlook.com>
2016-02-18 21:36:18 -0800

    Setup login/register

--- client/containers/Name.js
+++ client/containers/Name.js
@@ -2,9 +2,13 @@ import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import hashColor from '../hashColor';
-import * as Actions from '../actions';
-import {BEFORE_CHOOSE_NAME, DURING_CHOOSE_NAME, AFTER_CHOOSE_NAME} from '../constants/chooseName';
 import toId from '../../common/toId';
+import * as Actions from '../actions';
+import {
+  BEFORE_CHOOSE_NAME,
+  DURING_CHOOSE_NAME,
+  DURING_CHOOSE_AUTH_NAME,
+  AFTER_CHOOSE_NAME} from '../constants/chooseName';
 
 class Name extends Component {
   constructor(props) {
@@ -74,6 +78,24 @@ class Name extends Component {
           </div>
         </form>
       );
+    } else if (chooseName === DURING_CHOOSE_AUTH_NAME) {
+      formBody = (
+        <form className="navbar-form navbar-right">
+          <div className="form-group">
+            <button className="btn btn-default" onClick={(e) => {
+              e.preventDefault();
+              actions.setChooseName(AFTER_CHOOSE_NAME);
+            }}>
+              <span className="glyphicon glyphicon-chevron-left"></span>
+            </button>
+            {' '}
+            <input type="text" className="form-control" placeholder="Username" ref="username_input" />{' '}
+            <input type="text" className="form-control" placeholder="Password" ref="password_input" />{' '}
+            <button className="btn btn-primary" onClick={null}>Login</button>
+            <button className="btn btn-default" onClick={null}>Register</button>
+          </div>
+        </form>
+      );
     } else if (chooseName === AFTER_CHOOSE_NAME) {
       formBody = (<ul className="nav navbar-nav navbar-right">
         <li className="dropdown">
@@ -83,7 +105,7 @@ class Name extends Component {
           </a>
           <ul className="dropdown-menu">
             <li><a href="#" onClick={() => actions.setChooseName(DURING_CHOOSE_NAME)}>Change Name</a></li>
-            <li><a href="#" onClick={null}>Login/Register</a></li>
+            <li><a href="#" onClick={() => actions.setChooseName(DURING_CHOOSE_AUTH_NAME)}>Login/Register</a></li>
           </ul>
         </li>
       </ul>);
