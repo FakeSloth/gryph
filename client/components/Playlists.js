336332715e5d7c0794caa7cf623f9888c4b87453
Phil <birkal@outlook.com>
2016-02-25 22:10:50 -0800

    Add other routes

---
+++ client/components/Playlists.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Playlists = () => {
+  return (
+    <div>
+      <p>This feature is a work in progress!</p>
+    </div>
+  );
+};
+
+export default Playlists;
--- client/containers/Root.js
+++ client/containers/Root.js
@@ -2,6 +2,8 @@ import React, {PropTypes} from 'react';
 import {Provider} from 'react-redux';
 import {Router, IndexRoute, Route, browserHistory} from 'react-router';
 import App from '../components/App';
+import About from '../components/About';
+import Playlists from '../components/Playlists';
 import Home from './Home';
 
 const Root = ({store}) => {
@@ -10,6 +12,8 @@ const Root = ({store}) => {
       <Router history={browserHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={Home} />
+          <Route path="about" component={About} />
+          <Route path="playlists" component={Playlists} />
         </Route>
       </Router>
     </Provider>
