37fccbd83bc9b663dcfaf50b1b9f238c2aa011d0
Phil <birkal@outlook.com>
2016-02-28 00:06:12 -0800

    Use multiline strings

--- server/commands.js
+++ server/commands.js
@@ -5,11 +5,11 @@ const hashColor = require('../client/hashColor');
 
 module.exports = {
   hello(target, user) {
-    this.sendReply('Hello ' + user.name + '! You put ' + target + '.');
+    this.sendReply(`Hello ${user.name}! You put ${target}.`);
   },
 
   me(target, user) {
-    const dot = '<strong style="color:' + hashColor(user.userId) + '" >•</strong>';
-    this.add(dot + ' ' + user.name + ' <em>' + escapeHtml(target) + '</em>');
+    const dot = `<strong style='color: ${hashColor(user.userId)}'>•</strong>`;
+    this.add(`${dot} ${user.name} <em>${escapeHtml(target)}</em>`);
   }
 };
