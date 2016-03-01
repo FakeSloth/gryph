8cbb61779ec16d315c95340255e33d4db7491b77
Phil <birkal@outlook.com>
2016-02-29 21:36:10 -0800

    Update user commands help

--- server/commands.js
+++ server/commands.js
@@ -16,33 +16,39 @@ module.exports = {
 
   staff(target, room, user) {
     if (!this.isRank('staff')) return;
-    if (!target) return this.sendReply('/staff [user] - Promote a user to staff.');
+    if (!target) return this.sendReply('/staff [user] - Change a user\'s rank to staff.');
     room.rankUser(target, 5, 'Staff');
   },
 
   admin(target, room, user) {
     if (!this.isRank('staff')) return;
-    if (!target) return this.sendReply('/admin [user] - Promote a user to admin.');
+    if (!target) return this.sendReply('/admin [user] - Change a user\'s rank to admin.');
     room.rankUser(target, 4, 'Adminstrator');
   },
 
   globalmod: 'gmod',
   gmod(target, room, user) {
     if (!this.isRank('admin')) return;
-    if (!target) return this.sendReply('/gmod [user] - Promote a user to global moderator.');
+    if (!target) return this.sendReply('/gmod [user] - Change a user\'s rank to global moderator.');
     room.rankUser(target, 3, 'Global Moderator');
   },
 
   moderator: 'mod',
   mod(target, room, user) {
     if (!this.isRank('admin')) return;
-    if (!target) return this.sendReply('/mod [user] - Promote a user to moderator.');
+    if (!target) return this.sendReply('/mod [user] - Change a user\'s rank to moderator.');
     room.rankUser(target, 2, 'Moderator');
   },
 
   voice(target, room, user) {
     if (!this.isRank('gmod')) return;
-    if (!target) return this.sendReply('/voice [user] - Promote a user to voice.');
+    if (!target) return this.sendReply('/voice [user] - Change a user\'s rank to voice.');
     room.rankUser(target, 1, 'Voice');
+  },
+
+  reg(target, room, user) {
+    if (!this.isRank('gmod')) return;
+    if (!target) return this.sendReply('/reg [user] - Change a user\'s rank to a regular user.');
+    room.rankUser(target, 0, 'Regular User');
   }
 };
