14758d8cfebcca01b4bf899a73aabc0452f807cc
Phil <birkal@outlook.com>
2016-03-02 20:35:18 -0800

    Move socket global from index.html to client index

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
+import io from 'socket.io-client';
 import Root from './containers/Root';
 import configureStore from './store/configureStore';
+
+const socket = io();
+global.socket = socket;
 
 const store = configureStore();
 
