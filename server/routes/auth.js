e7287477ceb3f78e0c97a14c4170c25e6d73ab50
Phil <birkal@outlook.com>
2016-03-07 19:32:49 -0800

    Use get instead of has method due to machdb compatability

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,7 @@ const db = require('../db');
 const toId = require('toid');
 
 function auth(req, res) {
-  if (db('users').has(toId(req.body.name))) {
+  if (db('users').get(toId(req.body.name))) {
     return res.json({msg: 'This username is registered.'});
   }
   return res.json({success: true});
